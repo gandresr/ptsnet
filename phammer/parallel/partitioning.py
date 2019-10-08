0504150f85375ad4d925a2a5ffedc723ca925ff1
gr24269 <griano@utexas.edu>
2019-10-08 17:31:43 -0500

    Added function for even partitioning

---
+++ phammer/parallel/partitioning.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def even(N, k):
+    p = np.ones(N); n = N // k; r = N % k
+    for i in range(k):
+        start = i*n
+        end = start + n
+        if i < r:
+            start += i; end += i
+        elif r > 0:
+            start += r
+        p[start:end+1] = i
+    return p
