42ee02f49eb08ccb923a3bccceb4331121bb4d0f
gr24269 <griano@utexas.edu>
2020-01-06 23:25:19 -0600

    created test to extract processors associated to pipes in BWSN

---
+++ testing/test_get_bwsn.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import wntr
+import tsnet
+
+from phammer.simulation.sim import HammerSimulation
+from time import time
+
+duration = 0.00048259999999999997*3; time_step = 1
+inpfile = '/home/watsup/Documents/Github/phammer/example_files/BWSN1.inp'
+
+sim = HammerSimulation(
+    inpfile,
+    {
+        'time_step' : time_step,
+        'duration' : duration,
+        'skip_compatibility_check' : False,
+    },
+    period = 0,
+    default_wave_speed = 1200)
+
+sim.add_curve('V_BUTTERFLY', 'valve',
+    [1, 0.8, 0.6, 0.4, 0.2, 0],
+    [1e3, 0.044, 0.024, 0.011, 0.004, 0.   ])
+
+valves = sim.wn.valve_name_list
+sim.assign_curve_to('V_BUTTERFLY', valves)
+
+sim.initialize()
+
+idx = np.cumsum(sim.ic['pipe'].segments+1).astype(int)
+
+with open('pipe_processors_r.csv' % sim.worker.rank, 'w') as f:
+    for i, pipe in enumerate(sim.wn.pipe_name_list):
+        print(i, len(sim.wn.pipe_name_list))
+        if i > 0:
+            proc_pipe = sim.worker.processors[idx[i-1]:idx[i]]
+        else:
+            proc_pipe = sim.worker.processors[:idx[i]]
+        counts = np.bincount(proc_pipe)
+        f.write( "%s,%d\n" % (sim.ic['pipe'].ival(i), np.argmax(counts)) )
