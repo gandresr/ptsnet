476dfb743dd5b7ded9dd2d05510be95e77329f6b
gr24269 <griano@utexas.edu>
2020-02-26 17:35:40 -0600

    Script to plot profiling times

--- testing/get_plots.py
+++ testing/get_plots.py
@@ -2,7 +2,9 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-folders = os.listdir('results/BWSN_F')
+from phammer.arrays import Table, Table2D
+
+folders = os.listdir(os.getcwd())
 p = []
 get_partition = []
 check_processor_innactivity = []
@@ -17,33 +19,41 @@ run_boundary_step = []
 run_valve_step = []
 run_pump_step = []
 store_results = []
-barrier = []
+barrier1 = []
+barrier2 = []
 exchange_data = []
 
+cpath = os.getcwd()
+num_steps = int(cpath[cpath.rfind('_')+1:])
 for folder in folders:
-    if '.' in folder: continue
-    with open('results/BWSN_F/' + folder + '/summary.dat', 'rb') as f:
-        data = pickle.load(f)
-    p.append(int(folder[folder.find('_') + 1:]))
-    get_partition.append(data['get_partition'][-1])
-    check_processor_innactivity.append(data['check_processor_innactivity'][-1])
-    create_selectors.append(data['_create_selectors'][-1])
-    comm_graph.append(data['_define_dist_graph_comm'][-1])
-    allocate_memory.append(data['_allocate_memory'][-1])
-    load_initial_conditions.append(data['_load_initial_conditions'][-1])
-    total_sim_time.append(data['total_sim_time'][-1])
-    run_step.append(data['run_step'][-1])
-    run_interior_step.append(data['run_interior_step'][-1])
-    run_boundary_step.append(data['run_boundary_step'][-1])
-    run_valve_step.append(data['run_valve_step'][-1])
-    run_pump_step.append(data['run_pump_step'][-1])
-    store_results.append(data['store_results'][-1])
-    if p[-1] > 1:
-        barrier.append(data['barrier'][-1])
-        exchange_data.append(data['exchange_data'][-1])
-    else:
-        barrier.append(0)
-        exchange_data.append(0)
+    if '_' in folder and not '.' in folder:
+        print(folder)
+        with open(folder + '/durations.dat', 'rb') as f:
+            data = pickle.load(f)
+        num_processors = int(folder[folder.find('_') + 1:])
+        p.append(num_processors)
+
+        get_partition.append(data['get_partition'])
+        check_processor_innactivity.append(data['check_processor_innactivity'])
+        create_selectors.append(data['_create_selectors'])
+        comm_graph.append(data['_define_dist_graph_comm'])
+        allocate_memory.append(data['_allocate_memory'])
+        load_initial_conditions.append(data['_load_initial_conditions'])
+        total_sim_time.append(data['total_sim_time'])
+        run_step.append(data['run_step'])
+        run_interior_step.append(data['run_interior_step'])
+        run_boundary_step.append(data['run_boundary_step'])
+        run_valve_step.append(data['run_valve_step'])
+        run_pump_step.append(data['run_pump_step'])
+        store_results.append(data['store_results'])
+        if p[-1] > 1:
+            barrier1.append(data['barrier1'])
+            exchange_data.append(data['exchange_data'])
+            barrier2.append(data['barrier2'])
+        else:
+            barrier1.append(0)
+            barrier2.append(0)
+            exchange_data.append(0)
 
 plt.figure()
 temp = np.argsort(p)
@@ -62,17 +72,17 @@ bars4 = np.add(bars3, allocate_memory)
 p6 = plt.bar(X, load_initial_conditions, bottom = bars4, width = 0.5)
 bars5 = np.add(bars4, load_initial_conditions)
 
-p7 = plt.bar(X, np.array(run_interior_step)*850, bottom = bars5, width = 0.5)
-bars6 = np.add(bars5, np.array(run_interior_step)*850)
-p8 = plt.bar(X, np.array(run_boundary_step)*850, bottom = bars6, width = 0.5)
-bars7 = np.add(bars6, np.array(run_boundary_step)*850)
-p9 = plt.bar(X, np.array(run_valve_step)*850, bottom = bars7, width = 0.5)
-bars8 = np.add(bars7, np.array(run_valve_step)*850)
-p10 = plt.bar(X, np.array(run_pump_step)*850, bottom = bars8, width = 0.5)
-bars9 = np.add(bars8, np.array(run_pump_step)*850)
-p11 = plt.bar(X, np.array(store_results)*850, bottom = bars9, width = 0.5)
-bars10 = np.add(bars9, np.array(store_results)*850)
-p12 = plt.bar(X, np.array(barrier)*850*2+np.array(exchange_data)*850, bottom = bars10, width = 0.5)
+p7 = plt.bar(X, np.array(run_interior_step), bottom = bars5, width = 0.5)
+bars6 = np.add(bars5, np.array(run_interior_step))
+p8 = plt.bar(X, np.array(run_boundary_step), bottom = bars6, width = 0.5)
+bars7 = np.add(bars6, np.array(run_boundary_step))
+p9 = plt.bar(X, np.array(run_valve_step), bottom = bars7, width = 0.5)
+bars8 = np.add(bars7, np.array(run_valve_step))
+p10 = plt.bar(X, np.array(run_pump_step), bottom = bars8, width = 0.5)
+bars9 = np.add(bars8, np.array(run_pump_step))
+p11 = plt.bar(X, np.array(store_results), bottom = bars9, width = 0.5)
+bars10 = np.add(bars9, np.array(store_results))
+p12 = plt.bar(X, np.array(barrier1) + np.array(exchange_data) + np.array(barrier2), bottom = bars10, width = 0.5)
 plt.xticks(np.arange(len(p)), list(map(str, np.sort(p))))
 
 plt.legend(
@@ -95,3 +105,36 @@ plt.ylabel('Time [s]')
 plt.title('Simulation times by subprocess')
 plt.grid(True)
 plt.show()
+
+# files = os.listdir()
+# size_of_problem = {}
+# for file in files:
+#     if 'test_phammer.o' in file:
+#         extra_points = []
+#         with open(file, 'r') as f:
+#             for line in f:
+#                 if 'EXTRA' in line:
+#                     extra_points.append(float(line[:line.find('EXTRA')]))
+#         extra_points = np.array(extra_points)
+#         pp = len(extra_points)
+#         N = sum(extra_points/(1 - 1/pp))
+#         size_of_problem[pp] = N*num_steps
+
+# xx = list(X)
+# idx = xx.index(0)
+# print(np.array(p), 'processors')
+# plt.bar(X, total_sim_time[idx]/(np.array(total_sim_time)))
+# plt.xlabel('Processors')
+# plt.ylabel('Simulation Time')
+# plt.title('Speedup with MPI')
+# plt.xticks(np.arange(len(p)), list(map(str, np.sort(p))))
+# plt.show()
+
+# XX = np.array(list(size_of_problem.keys()))
+# YY = np.array(list(size_of_problem.values()))
+# order = np.argsort(XX)
+# plt.plot(XX[order], YY[order], '.')
+# plt.xlabel('Processors')
+# plt.ylabel('Problem Size')
+# plt.title('Problem size')
+# plt.show()
