669a4287628e90e88288f81e780a32798793055d
gr24269 <griano@utexas.edu>
2020-07-14 17:39:18 -0500

    Testing zarr array first

---
+++ testing/storage/save_load_zarr.py
@@ -0,0 +1,30 @@
+import numpy as np
+import os
+import zarr
+
+from phammer.arrays import ZarrArray
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.rank
+num_processors = comm.size
+
+num_points = 300000
+T = 100
+
+num_elements = num_points // num_processors
+zarr_shape = (num_points, T)
+comm.barrier()
+store = zarr.DirectoryStore(os.path.join(os.getcwd(), 'zarr_array', 'x.zarr'))
+data = np.random.rand(num_elements, T)
+try:
+    z = zarr.open(store, 'w', shape = zarr_shape, chunks = (1, zarr_shape[1],), dtype = float)
+except:
+    print(rank)
+    z = None
+chunk_size = data.shape[0]
+final_index = comm.scan(chunk_size)
+initial_index = final_index - chunk_size
+print(rank, initial_index, final_index, chunk_size, z.shape, data.shape)
+if z:
+    z[initial_index:final_index,:] = data
