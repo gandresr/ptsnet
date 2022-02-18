ad5d2aba26434fc3268f6adf0c3038d9ca61f695
gr24269 <griano@utexas.edu>
2022-02-17 21:42:04 -0600

    updated v0.0.19 (MANIFEST.in for Linux now)

--- tests/test_num_processors.py
+++ tests/test_num_processors.py
@@ -11,4 +11,4 @@ sim = PTSNETSimulation(
         'wave_speed_method' : 'user'
 })
 
-compute_num_processors(sim, plot=True, count=4, steps=2000)
+compute_num_processors(sim, plot=True, count=4, steps=2000, nprocessors=32)
