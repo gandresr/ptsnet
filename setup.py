fe19e51c87ca6b50eabe5d722bf66bc06997cd8b
gr24269 <griano@utexas.edu>
2022-07-17 04:52:17 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ptsnet",
-    version="0.0.25",
+    version="0.1.0",
     author="Gerardo Riano",
     author_email="griano@utexas.edu",
     description="Parallel Transient Simulation in Water Networks",
